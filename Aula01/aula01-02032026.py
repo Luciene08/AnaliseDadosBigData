083797d137c61c13e4256ded1a2a9009199158f1
Luciene08 <lucienelu1994@gmail.com>
2026-03-09 21:24:02 -0300

    adiciona aula 01

---
+++ Aula01/aula01-02032026.py
@@ -0,0 +1,41 @@
+# #AULA 01 - Variáveis
+
+# #tipos de Dados
+
+# print('olá mundo!') #Comando para imprimir uma resposta no terminal
+
+
+# pokebola01='Bulbasaur' #string
+# pokebola02='Psyduck'#string
+# pokebola03='Clefairy'
+# idade=30
+# nome='Maria'
+# preco=19.99
+# numerico=1986128
+# data='04-03-2026'
+# vazio=None
+# decisao1=True
+# decisao2=False
+
+# print(decisao1,decisao2)
+
+
+# print(type(preco))
+
+# #Boletim
+# bimestre1=9
+# bimestre2=7
+# media=(bimestre1+bimestre2)/2
+# print(media)
+
+##calculadora
+num1=3
+num2=4
+soma=num1+num2
+multiplicacao=num1*num2
+divisao=num1/num2
+subtracao=num1-num2
+modulo=num1%num2
+print(soma,multiplicacao,divisao,subtracao,modulo)
+
+

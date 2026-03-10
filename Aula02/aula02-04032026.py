513c5c665a859be8d5b3b60e04df73305e69e14a
Luciene08 <lucienelu1994@gmail.com>
2026-03-09 21:27:28 -0300

    adiciona Aula02

---
+++ Aula02/aula02-04032026.py
@@ -0,0 +1,85 @@
+##Estruturas de DECISÃO:
+
+##Exemplo 1
+
+# chover=True
+# rafa_presenca=None
+
+# if chover==True:
+#  rafa_presenca=False
+
+# else:
+#  rafa_presenca=True
+
+
+#  ##Exemplo 2:
+
+#  placar_time1=None
+#  placar_time2=None
+#  resultado=None
+
+#  if placar_time1 > placar_time2: ##Identação
+#   resultado='Time 1 ganhou!'
+#  elif placar_time1 < placar_time2:
+#   resultado='Time 2 ganhou!'
+#  else:resultado='Empate!'
+
+
+# a=10
+# b=5
+# print(f"soma: {a+b}\n")
+# print("subtração:", a - b)
+# print("multiplicação:", a * b)
+# print("divisão:", a/ b)
+
+# primeiro_nome = "Maria"
+# sobrenome= "Silva"
+# nome_completo = primeiro_nome + " "+ sobrenome
+# print(nome_completo)
+# print("O tipo armazenado|\n"
+#       f"na variável'primeiro_nome' é {type(primeiro_nome)}")
+
+# x = 21
+# y = 30
+# print("x é maior que y?", x > y)
+# print("x é igual a y?", x==y)
+
+# tem_carteira = True
+# idade = 18
+# tem_carro = False
+# bebe=False
+# pode_dirigir = (idade >= 18 and tem_carteira) and bebe==False
+# print("pode dirigir?", pode_dirigir)
+# print("pode dirigir e tem carro?", pode_dirigir and tem_carro)
+
+# frase = "Python é divertido"
+# print(frase.upper())
+# print(frase.lower())
+# nova_frase = frase.replace("divertido" , "poderoso")
+# print(nova_frase)
+
+itens_carrinho=2
+itens_carrinho=itens_carrinho+1
+print(itens_carrinho)
+
+
+contador = 0
+contador += 5 # contador=contador+5 <<<< Incremento
+contador -= 2 # contador=contador-2
+contador *= 3 # contador=contador*3
+print("Valor final do contador:" , contador)
+
+a =  int(input("digite uma nota:"))
+b = 20
+c = 30
+media= (a+b+c)/3
+print("média:", media)
+print("A média é maior que 15 e menor que 25?", 15< media<25)
+
+
+# nota= int(input("nota:"))
+#print(nota)
+
+#### TEREMOS MAIS TESTES???
+
+ 

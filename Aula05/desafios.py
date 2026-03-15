aeea0232f3265c54023215ffc62c87d2b7e5e921
Luciene08 <lucienelu1994@gmail.com>
2026-03-15 18:28:22 -0300

    adiciona aula

---
+++ Aula05/desafios.py
@@ -0,0 +1,99 @@
+
+##desafio 4
+
+# try:
+ 
+#     numero_codigo = int(input("Digite um numero de 1 a 11: "))
+
+#     match numero_codigo:
+#         case 1:
+#             print("Sul")
+#         case 2:
+#             print("Norte")
+#         case 3:
+#             print("Leste") 
+#         case 4:
+#             print("Oeste")
+#         case 5:
+#             print("Nordeste")
+#         case 6:
+#             print("Nordeste")
+#         case 7:
+#             print("Sudeste")
+#         case 8:
+#             print("Sudeste")
+#         case 9:
+#             print("Sudeste")
+#         case 10:
+#             print("Centro-Oeste")
+#         case 11:
+#             print("Noroeste")
+#         case _ :
+#             print("importado")
+# except ValueError:
+#     print("Importado")
+
+#### desafio 2##################
+
+largura= float(input("largura em metros: "))
+comprimento= float(input("comprimenro em metros: "))
+altura= float(input("altura em metros: "))
+print()#coloquei esse print pra pular uma linha no terminal
+dimensao_cozinha = largura * comprimento * altura
+print("Dimensão da cozinha:",dimensao_cozinha)
+
+caixa_azulejo=1.5
+quantidade_caixas= dimensao_cozinha / caixa_azulejo
+print("Quantidade de caixas:",quantidade_caixas)
+
+######desafio 3#################################
+
+#Odometro
+# odometro_inicial=float(input("odometro inicial: "))
+# km_dia=float(input("km rodado: "))
+# odometro_final=odometro_inicial+km_dia
+# print("odometro", odometro_final)
+
+# combustível
+
+# combustivel_inicial= float(input("combustivel inicial em litros: "))
+# combustivel_final= float(input("combustivel fim do dia em litros: "))
+# gasto_combustivel=combustivel_inicial-combustivel_final
+# print("gasto total de combustivel: ",gasto_combustivel)
+# valor_combustive=6.15
+# valor_gasto= gasto_combustivel*valor_combustive
+# print("valor gasto com combustivel: ",valor_gasto)
+# print()##espassamento
+# media_de_consumo=km_dia/gasto_combustivel
+# print("media de consumo de combustivel gasto: ",media_de_consumo)
+# print()##espassamento
+# valor_recebido_corrida=float(input("valor recebido no fim do dia: "))
+# lucro=valor_recebido_corrida-valor_gasto
+# print("lucro do dia: ",lucro)
+
+
+
+#### desafio 6####
+
+# numero = int(input("um numero:"))
+# if numero >=0:
+#  print("positivo")
+# elif numero <= 2:
+#  print("negativo")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
